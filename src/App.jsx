a97e04f1103f041ef93cdcc63f32c9cabdbcf4f4
fdhani <39753107+fdhani@users.noreply.github.com>
2023-11-28 20:27:05 +0700

    fix(server): remove unused files

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Routes from "./pages";
-const router = createBrowserRouter(Routes);
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
